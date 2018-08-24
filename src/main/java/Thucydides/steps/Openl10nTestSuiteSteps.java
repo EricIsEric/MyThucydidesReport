a6fa4867b49ebdbc98c74e92b14d79292b28dd2d
Eric <wangyongguang1029@163.com>
2018-08-24 14:42:36 +0800

    RunExeMaint20

--- src/main/java/Thucydides/steps/Openl10nTestSuiteSteps.java
+++ src/main/java/Thucydides/steps/Openl10nTestSuiteSteps.java
@@ -77,8 +77,9 @@ public class Openl10nTestSuiteSteps {
 		logger.info("Into:  RunRisk1Exe()----------------------------");
 		Thread.sleep(5000);
 		File file = new File("src/test/resources/autoItScript/Risk01_EmptyZipFile/PPM_9.50_fi.exe");
-		browserObjectAndCommonMethodPage.executeCmd(file.getAbsolutePath().replace('\\', '/'));
-		java.awt.Desktop.getDesktop().open(file);  //这里或许可以不用open file
+		browserObjectAndCommonMethodPage.executeCmd("powershell.exe Start-Process PPM_9.50_fi_EmptyZip.exe -verb RunAs");
+//		browserObjectAndCommonMethodPage.executeCmd(file.getAbsolutePath().replace('\\', '/'));
+//		java.awt.Desktop.getDesktop().open(file);  //这里或许可以不用open file
 		/*logger.info("=-=-=-=-=-=-Starting Runtime=-=-=-=-=-=-");
 		Thread.sleep(10000);
 		Runtime run = Runtime.getRuntime();
